3f51a4beef8b5867eb8e820ef2eaea82158e72c7
i am dk <iamdk353@gmail.com>
2024-03-24 11:51:04 +0530

    skills section finished

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,30 @@
+import skills from "./iterators";
+
 const Skills = () => {
   return (
-    <div className="flex justify-center mt-4" id="skills">
+    <div className="flex items-center my-4 flex-col" id="skills">
       <p className="sora text-3xl">
         MY <span className="font-bold"> SKILLS</span>
       </p>
-      <div></div>
+      <div
+        className="w-[80%] 3
+       my-5 grid grid-cols-2 p-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-10"
+      >
+        {skills.map(({ src, name, learning }) => {
+          return (
+            <div className={`${learning ? "tooltip" : ""}`} data-tip="learning">
+              <div
+                className={`size-[15vh] bg-white flex flex-col p-2 justify-center items-center p-5 border-black border cursor-default ${
+                  learning ? "opacity-70 animate-pulse " : ""
+                }`}
+              >
+                <img src={src} alt={name + " icon"} className="size-10" />
+                <p className=" text-xs md:text-sm mt-2">{name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/components/iterators.ts
@@ -0,0 +1,82 @@
+import git from "../assets/icon-git.svg";
+import express from "../assets/icon-express.svg";
+import next from "../assets/icon-nextjs.svg";
+import js from "../assets/icon-javscript.svg";
+import mongo from "../assets/icon-mongodb.svg";
+import node from "../assets/icon-nodejs.svg";
+import react from "../assets/icon-react.svg";
+import tailwind from "../assets/icon-tailwindcss.svg";
+import tsIcon from "../assets/typeIcon.svg";
+import gitHub from "../assets/github-mark.png";
+import cpp from "../assets/cpp.svg";
+import c from "../assets/c.svg";
+import py from "../assets/py.png";
+const skills = [
+  {
+    src: express,
+    name: "Express",
+    learning: false,
+  },
+  {
+    src: js,
+    name: "Javascript",
+    learning: false,
+  },
+  {
+    src: mongo,
+    name: "MongoDB",
+    learning: false,
+  },
+  {
+    src: node,
+    name: "Node",
+    learning: false,
+  },
+  {
+    src: tailwind,
+    name: "Tailwind Css",
+    learning: false,
+  },
+  {
+    src: cpp,
+    name: "C++",
+    learning: false,
+  },
+  {
+    src: c,
+    name: "C",
+    learning: false,
+  },
+  {
+    src: py,
+    name: "Python",
+    learning: false,
+  },
+  {
+    src: git,
+    name: "GIT",
+    learning: true,
+  },
+  {
+    src: next,
+    name: "Next",
+    learning: true,
+  },
+  {
+    src: react,
+    name: "React",
+    learning: true,
+  },
+  {
+    src: tsIcon,
+    name: "Type Script",
+    learning: true,
+  },
+  {
+    src: gitHub,
+    name: "GitHub",
+    learning: true,
+  },
+];
+
+export default skills;
