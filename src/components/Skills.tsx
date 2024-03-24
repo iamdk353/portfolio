ae2b940f8a5c716d404871fad409c624ae79cd52
i am dk <iamdk353@gmail.com>
2024-03-24 10:16:50 +0530

    starting skills section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,11 @@
+const Skills = () => {
+  return (
+    <div className="flex justify-center mt-4" id="skills">
+      <p className="sora text-3xl">
+        MY <span className="font-bold"> SKILLS</span>
+      </p>
+      <div></div>
+    </div>
+  );
+};
+export default Skills;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import "./index.css";
+import Skills from "./components/Skills";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ function App() {
     <div className="p-3 md:p-10">
       <Nav />
       <Hero />
+      <Skills />
     </div>
   );
 }
